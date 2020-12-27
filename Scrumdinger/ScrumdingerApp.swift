0d58c08e2bd92c548a93607510e9117cf1f15cbe
Scott Swezey <129733+scottswezey@users.noreply.github.com>
2020-12-27 14:53:15 -0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Scott on 12/27/20.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
