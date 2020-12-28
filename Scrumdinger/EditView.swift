76333954679faff258fb83a86c12a5ebbd0e0424
Scott Swezey <129733+scottswezey@users.noreply.github.com>
2020-12-27 21:44:18 -0800

    Completed tutorial section: "Passing Data with Bindings"

--- Scrumdinger/EditView.swift
+++ Scrumdinger/EditView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct EditView: View {
-	@State private var scrumData = DailyScrum.Data()
+	@Binding var scrumData: DailyScrum.Data
 	@State private var newAttendee = ""
 	
 	var body: some View {
@@ -61,6 +61,6 @@ struct EditView: View {
 
 struct EditView_Previews: PreviewProvider {
 	static var previews: some View {
-		EditView()
+		EditView(scrumData: .constant(DailyScrum.data[0].data))
 	}
 }
