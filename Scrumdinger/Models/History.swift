8b2d81a4a239611c0d1d084e3ea2c3fcbbc77af6
Scott Swezey <129733+scottswezey@users.noreply.github.com>
2020-12-27 22:41:28 -0800

    Update to match sample project.

---
+++ Scrumdinger/Models/History.swift
@@ -0,0 +1,22 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by Scott on 12/27/20.
+//
+
+import Foundation
+
+struct History: Identifiable {
+	let id: UUID
+	let date: Date
+	var attendees: [String]
+	var lengthInMinutes: Int
+	
+	init(id: UUID = UUID(), date: Date = Date(), attendees: [String], lengthInMinutes: Int) {
+		self.id 							= id
+		self.date 						= date
+		self.attendees 				= attendees
+		self.lengthInMinutes 	= lengthInMinutes
+	}
+}
